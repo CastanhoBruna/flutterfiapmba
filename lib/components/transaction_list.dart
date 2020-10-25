0c159126a8f6eb6c6ed817eb9cf659be16ce19bd
CastanhoBruna <brunaplantierpos@gmail.com>
2020-10-25 00:47:06 -0300

    .

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,8 +1,9 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 import '../utils/app_routes.dart';
-
+// import 'package:flutter/flare_flutter/flare_actor.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -10,26 +11,36 @@ class TransactionList extends StatelessWidget {
 
   TransactionList(this.transactions, this.onRemove);
 
+   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                Text(
-                  'Nenhum gasto cadastrado!',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                SizedBox(height: 20),
-                Container(
-                  height: 130,
-                  child: Image.asset(
-                    '../assets/images/costs.png',
-                    fit: BoxFit.cover,
+    // var _imagem;
+        return Container(
+          height: 300,
+          child: transactions.isEmpty
+              ? Column(
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    Text(
+                      'Nenhum gasto cadastrado!',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 130,
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                    child: Image.asset(
+                      '../assets/images/costs.png',
+                        //                 child: Image.asset(
+                        // '../assets/images/costs.png',
+                        fit: BoxFit.cover,
+
+                        ),
+                      )
                   ),
-                ),
+              
               ],
             )
           : ListView.builder(
@@ -71,3 +82,17 @@ class TransactionList extends StatelessWidget {
     );
   }
 }
+
+// class SpringCurve extends Curve {
+//   const SpringCurve({
+//     this.a = 0.15,
+//     this.w = 19.4,
+//   });
+//   final double a;
+//   final double w;
+
+//   @override
+//   double transformInternal(double t) {
+//     return -(pow(e, -t / a) * cos(t * w)) + 1;
+//   }
+// }
